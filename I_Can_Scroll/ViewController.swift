66e58a55b167e8566be7485b1aecb92197192d26
Majid <majid.iqbal54@gmail.com>
2017-01-01 19:07:02 +0500

    Scrolling App

--- I_Can_Scroll/ViewController.swift
+++ I_Can_Scroll/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var scrollView:UIScrollView!
+    
+    let WIDTH :CGFloat = 140
+    let HEIGHT :CGFloat = 131
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+      for x in 1...5 {
+        
+          let img = UIImage(named: "\(x)")
+          let imgView = UIImageView(image: img)
+        
+        scrollView.addSubview(imgView)
+        
+        imgView.frame = CGRect(x:-WIDTH + (180 * CGFloat(x)) , y:120 , width:WIDTH, height: HEIGHT)
+            
+        }
+        
+        scrollView.contentSize = CGSize(width: 180 * 5, height: HEIGHT)
+    
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
+    
 
 }
 
